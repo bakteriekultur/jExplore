aecd1c34c0da07518fb310163cec45481f10249e
Lundén Jakob <jakob.lunden2@skola.uppsala.se>
2020-09-12 11:05:59 +0200

    Start på configläsning samt uppdatering av config-filen

--- functions.h
+++ functions.h
@@ -20,6 +20,10 @@ struct folderStruct{
   int amountOfTags;
   struct folderSettingsStruct;
 };
+struct configStruct{
+  char paths[100][80];
+  struct folderStruct folders[100];
+};
 
 struct pathStruct readPath(char* pathStr){
   FILE *fp;
@@ -28,7 +32,7 @@ struct pathStruct readPath(char* pathStr){
   char c=0;
   int i=0;
   int j=0;
-  char command[sizeof(pathStr)+11];
+  char command[strlen(pathStr)+12];
   strcpy(command, "dir ");
   strcat(command, pathStr);
   strcat(command, " /a-d/b");
@@ -36,7 +40,7 @@ struct pathStruct readPath(char* pathStr){
     perror("popen() failed\n");
     exit(EXIT_FAILURE);
   }
-  memset(buffer, 0, sizeof(buffer));
+  memset(buffer, 0, strlen(buffer));
   while(fread(&c, sizeof(c), 1, fp)){
     if(c!='\n'){
       buffer[i]=c;
@@ -54,24 +58,96 @@ struct pathStruct readPath(char* pathStr){
   return path;
 }
 
+struct configStruct readConfig(char* configPath){
+  FILE *fp = fopen(configPath, "r");
+  struct configStruct config;
+  char buffer[200];
+  int c, i, bracket, hard;
+  br
+  i = 0;
+  bracket = 0;
+  hard = 0;
+  short mode = 0; //0 = utanför hakar, 1=paths, 2=folders, 3=settings
+  short submode = 0; //0 = inget läge, 1=titel/path, 2=tags, 3 = extentions, mer = settings
+  while((c=getc(fp))!=EOF){
+    buffer[i] = c;
+    switch(c){
+      case '[':hard++;break;
+      case ']':hard--;break;
+      case '{':bracket++;break;
+      case '}':bracket--;break;
+    }
+    switch(mode){
+      case 0: {
+                if(!strcmp(buffer, "#paths")){
+                    mode = 1;
+                    memset(buffer, 0, sizeof(buffer));
+                    i=0
+                }
+                if(!strcmp(buffer, "#folders")){
+                    mode = 2;
+                    memset(buffer, 0, sizeof(buffer));
+                    i=0
+                }
+                if(!strcmp(buffer, "#settings")){
+                    mode = 3;
+                    memset(buffer, 0, sizeof(buffer));
+                    i=0
+                }
+                hard=0;
+                bracket=0;
+                break;
+              }
+      case 1: {
+                if(hard==0){
+                  mode = 0;
+                  break;
+                }
+                switch(submode){
+                  case 0:{
+                    if(!strcmp(buffer, "path")){
+                      mode=1;
+                      memset(buffer, 0, sizeof(buffer));
+                      i=0;
+                    }
+                    if(!strcmp(buffer, "path")){
+                      mode=1;
+                      memset(buffer, 0, sizeof(buffer));
+                      i=0;
+                    }
+                    if(!strcmp(buffer, "path")){
+                      mode=1;
+                      memset(buffer, 0, sizeof(buffer));
+                      i=0;
+                    }
 
 
 
+              
 
 
 
+                }
+      case 2: {
 
 
 
+              
 
 
 
+                }
+      case 3: {
 
 
 
+              
 
 
 
+                }
+                }
+  return config;
+}
 
-
-
+  
